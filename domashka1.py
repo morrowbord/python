7f4be96d7e4cc604ecef7aaa465bee5bc40dddcb
Nail <morrowbord@mail.ru>
2020-12-12 14:32:51 +0500

    1 задание, 2урок

---
+++ domashka1.py
@@ -0,0 +1,10 @@
+string1 = 'ah', (5, 4, 3, 'df'), [3, 2, 1, 0, 'r'], 6453452, True, 0b0110, \
+          None, \
+          {3: 4, 1: 'd', 2: 'r', 't': 3}, {4, 3,
+                                           5,
+                                           'r',
+                                           'm',
+                                           '4'}
+# print(type(string1))
+for num in string1:
+    print(type(num))
