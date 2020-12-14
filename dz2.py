67366c353e897eba41c6e2d230253551a46a47af
morrowbord <74824619+morrowbord@users.noreply.github.com>
2020-12-14 13:49:14 +0500

    Delete dz2.py
    удалил с этой ветки

--- dz2.py
+++
@@ -1,5 +0,0 @@
-timer = int(input('введите время в секундах: '))
-hours = timer // 3600
-minutes = int(timer % 3600 / 60)
-seconds = abs(minutes - (timer % 3600 / 60))
-print(f'Время в часах, минутах и секундах: {hours}:{minutes}:{seconds*60:.0f}')
