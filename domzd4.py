f63924f5ec1de01c7e93a87d023eb4a8cc12c1de
Nail <morrowbord@mail.ru>
2020-12-16 23:58:21 +0500

    добавил пояснения к задаче 4, сменил тип на float

--- domzd4.py
+++ domzd4.py
@@ -2,5 +2,5 @@ def my_func(x, y):
     x = x ** (-y)
     return x
 
-
-print(f"X в степени -Y: {my_func(int(input('введите X: ')), int(input('введите Y: ')))}")
+print("Программа считает Х в степени (-Y):")
+print(f"X в степени (-Y): {my_func(float(input('введите X: ')), int(input('введите Y: '))):.5f}")
