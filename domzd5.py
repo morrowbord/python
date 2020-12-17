1e2cb8e0ba2dc17465c31e448af78042f7d404fe
Nail <morrowbord@mail.ru>
2020-12-18 00:01:52 +0500

    доработка 5 программы, с выходом по Q/q

--- domzd5.py
+++ domzd5.py
@@ -3,9 +3,9 @@ def func_key():
     s = 0
     while True:
         string1 = input("Введите числа для сложения через пробел, "
-                        "для выхода напишите 'q': ").split()
+                        "для выхода -'q': ").split()
         for number_i in string1:
-            if number_i == 'q':
+            if number_i == 'q' or number_i == "Q":
                 print('exit')
                 return s
             s = s + int(number_i)
