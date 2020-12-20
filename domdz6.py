b08bb368673aff99b0ee5b8dee6696adfa24a1d0
Nail <morrowbord@mail.ru>
2020-12-20 14:34:29 +0500

    6 задача, Final

--- domdz6.py
+++ domdz6.py
@@ -1,13 +1,15 @@
 def int_func():
-    global list_line
+    # global list_line
     text = input("введите строки: ").split()
     list_line =[]
     for i in text:
         i = i.capitalize()
     # print(i)
         list_line.append(i)
+    return list_line
 
 
-print(int_func())
+list_line = int_func()
+# print(list_line)
 rez = ' '.join(list_line)
 print(rez)
