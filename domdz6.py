73ad5a64731ee20d8b21883741660207380e787f
Nail <morrowbord@mail.ru>
2020-12-20 13:59:20 +0500

    Переделанная 6 задача, выводит верно строки + None

--- domdz6.py
+++ domdz6.py
@@ -1,15 +1,13 @@
 def int_func():
-    text = input("введите строки: ")
-    text = text.capitalize()
-    return text
-
-
-list1 = int_func().split()
-list_line =[]
-for i in list1:
-    i = i.capitalize()
+    global list_line
+    text = input("введите строки: ").split()
+    list_line =[]
+    for i in text:
+        i = i.capitalize()
     # print(i)
-    list_line.append(i)
-# print(list_line)
+        list_line.append(i)
+
+
+print(int_func())
 rez = ' '.join(list_line)
 print(rez)
