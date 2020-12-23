3cd2b6a5d2cdddaa4d8d65ca62b87342471d6066
Nail <morrowbord@mail.ru>
2020-12-23 20:54:12 +0500

    удалил лишнее из задачи

--- dom5.py
+++ dom5.py
@@ -6,7 +6,7 @@ def my_func(x, y):
     return x * y
 
 
-list1 = [i for i in range(100, 1001*2) if (i) % 2 == 0]
+list1 = [i for i in range(100, 1001) if (i) % 2 == 0]
 sum_all = reduce(my_func, list1)
 
 print(f"последовательность:\n{list1}\n")
