9bdb72cb6f4c649e3eb4053b41d8adaa64f57701
Nail <morrowbord@mail.ru>
2021-01-02 19:46:43 +0500

    удаление лишних файлов

--- dz4.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Введите число: "))
-max = 0
-while number:
-    number_i = number % 10 #вытаскивает  цифры с конца, т.к это осаток от деления на 10
-    if number_i > max:
-        max = number_i
-#    print(number_i)
-    number = (number // 10) #режет число,пока number не станет false
-print(f'самое большое число: {max} ')
