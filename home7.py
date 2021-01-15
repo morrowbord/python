c7fed0bb329d7412ee99b992ac0efa648841a46c
Nail <morrowbord@mail.ru>
2021-01-15 18:50:34 +0500

    сделан нормальный вывод в json, среднее значение выводится но не так как надо!

--- home7.py
+++ home7.py
@@ -3,6 +3,7 @@ import json
 with open("text777.json", "w", encoding="utf-8") as write_file:
     with open("text_7.txt", "r", encoding="utf-8") as t3_file:
         everage_profit = 0
+        final_result = {}
         i = 0
         for content in t3_file:
             profit = int(content.split()[2])-int(content.split()[3])
@@ -10,18 +11,12 @@ with open("text777.json", "w", encoding="utf-8") as write_file:
                 everage_profit = everage_profit + profit
                 i += 1
                 ev = (everage_profit) / i
-            ev = str(ev)
+                ev = str(ev)
 
             result = {content.split()[0]: profit}
-            result = {**result}
+            final_result = {**final_result,**result}
+
             # result = тут надо взять значения словаря и сделать один
             print(result)
-            final_result = [result, ev]
-            json.dump(final_result, write_file, ensure_ascii=False, indent=4)
-            # json.dump(result, write_file, ensure_ascii=False, indent=4)
-
-
-
-
-
-
+        final_result = [final_result,ev]
+        json.dump(final_result, write_file, ensure_ascii=False, indent=4)
