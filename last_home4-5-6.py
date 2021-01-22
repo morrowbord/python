58a33b66103822cac32e3c844ebbde7c99438ced
Nail Khabibullin <74824619+morrowbord@users.noreply.github.com>
2021-01-22 16:22:53 +0500

    Update last_home4-5-6.py
    code rewrited to data storage

--- last_home4-5-6.py
+++ last_home4-5-6.py
@@ -1,19 +1,13 @@
-# class ZeroEx(Exception):
-#     def __init__(self,txt):
-#         self.txt = txt
-
 class Sklad:
-    @staticmethod
-    def na_sklad():
-        list1 = [input(f"добавить на склад:").split()]
-        list1.append(list1)
-        return list1
-
+    def na_sklad(self, count):
+        count = [input(f"добавить на склад:").split()]
+        self.count = count
+        return count
 
 class Orgtech(Sklad):
     def __init__(self, x):
         self.x = x
-        print(f" {self.x} = нет на складе ")
+        print(f" добавлено {data_scaners} шт на склад *сканеров")
 
 
 class Scanner(Orgtech):
@@ -31,10 +25,17 @@ class Xerox(Orgtech):
         return f"{self.x}"
 
 
-scanner1 = Scanner("Сканер")
-printer1 = Printer("Принтер")
-xerox1 = Xerox("Ксерокс")
+data_scaners = []
+
+# scanner1 = Scanner("Сканер")
+# printer1 = Printer("Принтер")
+# xerox1 = Xerox("Ксерокс")
+
 
 while True:
-    print(f"на складе: принтеров {Sklad.na_sklad()[0][0]}, сканеров {Sklad.na_sklad()[0][0]}, "
-          f"ксероксов {Sklad.na_sklad()[0][0]}")
+    sc = Scanner(0)
+    sc.na_sklad(0)
+    data_scaners += [int(sc.count[0][0])]
+    print(data_scaners)
+    # pr = Printer(8)
+    # xe = Xerox(9)
